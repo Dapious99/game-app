d957aaeae6bc92d1b687aee83a4467e93adc07ed
Kodewarlock <dapious99@gmail.com>
2025-03-14 08:45:32 +0100

    added little styling to Game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,19 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Game Screen</Text>
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
