360b84c237ee23b5b7804874a437abd15cc984c5
Kodewarlock <dapious99@gmail.com>
2025-04-05 08:34:46 +0100

    additions

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,20 @@
 import { Text, View, StyleSheet } from "react-native";
 import Title from "../components/Title";
+import { useState } from "react";
 
-function GameScreen() {
+const generateRandomNumbers = (min, max, exclude) => {
+  const randomNumbers = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randomNumbers === exclude) {
+    return generateRandomNumbers(min, max, exclude);
+  } else {
+    return randomNumbers;
+  }
+};
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomNumbers(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState();
   return (
     <View style={styles.container}>
       <Title>Opponent's guess</Title>
